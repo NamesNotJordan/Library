7242590f52c2cb920cb6b13800c9465133774ac2
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-02-29 23:00:37 +0200

    added initializing of book and child elements. Still have to combine into one element and add to grid

--- library.js
+++ library.js
@@ -10,3 +10,33 @@ const myLibrary = [];
 function addBookToLibrary(book) {
     myLibrary.push(book)
 }
+
+function library_to_grid(){
+    //TODO: Create grid based on size of 
+    const grid = document.getElementById("library-grid")
+
+    for (let i = 0; myLibrary.length(); i++) {
+        // TODO: Create Book element
+        const book = document.createElement("div");
+        
+        const book_title = document.createElement("h2")
+        book_title.className = "book-title"
+        book_title.innerHTML = myLibrary[i].title;
+
+        const book_author = document.createElement("span")
+        book_author.className = "book-author"
+        book_author.innerHTML = myLibrary[i].author;
+
+        const book_pages = document.createElement("span")
+        book_pages.className = "book-pages"
+        book_pages.innerHTML = myLibrary[i].pages;
+
+        const book_read = document.createElement("span")
+        book_read.className = "book-read"
+        book_read.innerHTML = myLibrary[i].read;
+
+        
+        // TODO: Add Book to grid
+    }
+    
+}
