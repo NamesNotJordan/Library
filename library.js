0876d1e108921a1839b142c14aab1d10829a569c
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-04 20:52:02 +0200

    converted book making function to handle one book per function call

--- library.js
+++ library.js
@@ -11,58 +11,71 @@ class Library {
     constructor(){
         this.books = []
     }
-    
-    addBookToLibrary(book) {
-        this.books.push(book)
+    inLibrary(title) {
+        return this.books.some((book)=> book.title !== title)
+    }
+    addBookToLibrary(newBook) {
+        if (!this.inLibrary(newBook)){
+            this.books.push(book)
+        }        
     }
     removeFromLibrary(title) {
         this.books = this.books.filter((book) => book.title !== title)
     }
 }
+const library = new Library()
 
-
-function library_to_grid(){
-    //TODO: Create grid based on size of 
+const createBookCard = (book) =>{
     const grid = document.getElementById("library-grid")
-
-    for (let i = 0; myLibrary.length(); i++) {
-        // TODO: Create Book element
-        const book = document.createElement("div");
-        
-        const book_title = document.createElement("h2")
-        book_title.className = "book-title"
-        book_title.innerHTML = myLibrary[i].title;
-
-        const book_author = document.createElement("span")
-        book_author.className = "book-author"
-        book_author.innerHTML = `Author: ${myLibrary[i].author}`;
-
-        const book_pages = document.createElement("span")
-        book_pages.className = "book-pages"
-        book_pages.innerHTML = `${myLibrary[i].pages} pages`;
-
-        const book_read = document.createElement("span")
-        book_read.className = "book-read"
-        book_read.innerHTML = myLibrary[i].read;
-
-        const remove_book_btn = document.createElement("button")
-        remove_book_btn.className = "remove-book-btn"
-        remove_book_btn.innerHTML = "remove book"
-
-        const read_status_btn = document.createElement("button")
-        read_status_btn.className = "read_btn"
-        read_status_btn.innerHTML = "toggle read status"
-
-        //Assemble Book
-        book.appendChild(book_title);
-        book.appendChild(book_author);
-        book.appendChild(book_pages);
-        book.appendChild(book_read);
-        book.appendChild(remove_book_btn)
-        // TODO: Add Book to grid
-        grid.appendChild(book);
-    }
     
+    // TODO: Create Book element
+    const bookCard = document.createElement("div");
+    
+    const book_title = document.createElement("h2")
+    book_title.classList.add("book-title")
+    book_title.innerHTML = book.title
+
+    const book_author = document.createElement("span")
+    book_author.classList("book-author")
+    book_author.innerHTML = `Author: ${book.author}`;
+
+    const book_pages = document.createElement("span")
+    book_pages.classList.add("book-pages")
+    book_pages.innerHTML = `${book.pages} pages`;
+
+    const book_read = document.createElement("span")
+    book_read.classList.add("book-read")
+    book_read.innerHTML = book.read;
+
+    const btn_group = document.createElement("div")
+
+    const remove_book_btn = document.createElement("button")
+    remove_book_btn.classList.add("remove-book-btn")
+    remove_book_btn.innerHTML = "remove book"
+
+    const read_status_btn = document.createElement("button")
+    read_status_btn.classList.add("read_btn")
+    read_status_btn.innerHTML = "toggle read status"
+
+    //Assemble Book
+    bookCard.appendChild(book_title);
+    bookCard.appendChild(book_author);
+    bookCard.appendChild(book_pages);
+    bookCard.appendChild(book_read);
+    bookCard.appendChild(btn_group);
+    btn_group.appendChild(remove_book_btn)
+    btn_group.appendChild(read_status_btn)
+    // TODO: Add Book to grid
+    grid.appendChild(bookCard);
 }
 
-library_to_grid()
+
+// Events:
+
+const addBook = (e) => {
+
+} 
+
+const removeBook = (e) => {
+    
+}
