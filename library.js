2be0f7115f45e206904fa0b2b8c981b1ebd51a4c
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-13 21:16:13 +0200

    Added event listener to add book button to show form

--- library.js
+++ library.js
@@ -6,3 +6,17 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
 }
+function addBookToLibrary() {
+    // Getting The values from form
+    const title = document.querySelector("#title").value
+    const author = document.querySelector("#author").value
+    const pages = document.querySelector("#pages").value
+    const read = document.querySelector("#read").value
+    
+}
+
+const addBookButton = document.querySelector("#add-book-btn")
+addBookButton.addEventListener("click", function() {
+    const newBookForm = document.querySelector("#new-book-form")
+    newBookForm.style.display = "block"
+})
