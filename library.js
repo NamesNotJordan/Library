bb25d378e32e4f669a7a30a99ba567a2c2a96d7f
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-13 22:06:20 +0200

    added some style. Changed the renderLibrary function to use an iterator to make removal easier

--- library.js
+++ library.js
@@ -20,22 +20,29 @@ function addBookToLibrary() {
 function renderLibrary() {
     const libraryBooks = document.querySelector("#library")
     libraryBooks.innerHTML = ""
-    library.forEach(book => {
+    for (let i = 0; i < library.length; i++) {
+        const book = library[i];
         const bookCard = document.createElement("div")
         bookCard.innerHTML = `
             <h3 class="book-title>${book.title}</h3>
             <p class = "book-author">by ${book.author}</p>
             <p>${book.pages} pages</p>
             <p class="book-read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+            <button> class="remove-btn" onclick="removeBook(${i})
             `
         libraryBooks.appendChild(bookCard)
-    });
+    }    
+}
+
+function removeBook(index) {
+    library.splice(index, 1)
+    renderLibrary()
 }
 
 const addBookButton = document.querySelector("#add-book-btn")
 addBookButton.addEventListener("click", function() {
     const newBookForm = document.querySelector("#new-book-form")
-    newBookForm.style.display = "block"
+    newBookForm.style.display = "flex"
 })
 
 document.querySelector("#new-book-form").addEventListener("submit", function(event) {
