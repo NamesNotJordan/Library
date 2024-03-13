81713799e16571a9be2e811d2b2ca14572d3c317
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-13 21:53:36 +0200

    Added renderLibrary. Can now add books to array and render cards on page

--- library.js
+++ library.js
@@ -13,11 +13,32 @@ function addBookToLibrary() {
     const pages = document.querySelector("#pages").value
     const read = document.querySelector("#read").checked
     const newBook = new Book(title,author,pages,read)
-    library.push(newBook)    
+    library.push(newBook)  
+    renderLibrary()  
+}
+
+function renderLibrary() {
+    const libraryBooks = document.querySelector("#library")
+    libraryBooks.innerHTML = ""
+    library.forEach(book => {
+        const bookCard = document.createElement("div")
+        bookCard.innerHTML = `
+            <h3 class="book-title>${book.title}</h3>
+            <p class = "book-author">by ${book.author}</p>
+            <p>${book.pages} pages</p>
+            <p class="book-read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+            `
+        libraryBooks.appendChild(bookCard)
+    });
 }
 
 const addBookButton = document.querySelector("#add-book-btn")
 addBookButton.addEventListener("click", function() {
     const newBookForm = document.querySelector("#new-book-form")
     newBookForm.style.display = "block"
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", function(event) {
+    event.preventDefault()
+    addBookToLibrary()
 })
