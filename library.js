bc41c5556bf6c4819bc3f4335f9562f604769fc1
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-22 16:35:37 +0200

    Created Book class that includes constructor and toggle read method

--- library.js
+++ library.js
@@ -1,10 +1,16 @@
 const library = []
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
 Book.prototype.toggleRead = function(){
